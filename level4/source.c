13780ca341fd11900de57c16837498308c5c1db6
themarch <theomarchandarvierlyon@gmail.com>
2020-11-19 17:46:51 +0100

    create level4

---
+++ level4/source.c
@@ -0,0 +1,17 @@
+void p(char *format)
+{
+    printf(format);
+    return;
+}
+
+void n(void)
+{
+    char *s;
+    
+    fgets(&s, 0x200, _reloc.stdin);
+    p((char *)&s);
+    if (_m == 0x1025544) {
+        system("/bin/cat /home/user/level5/.pass");
+    }
+    return;
+}
